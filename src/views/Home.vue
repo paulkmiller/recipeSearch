2f1fa4192731ce15b8baf3bdd93e1d8c7322a9aa
Paul Miller <paulmiller@Pauls-MacBook-Pro.local>
2023-03-10 13:35:11 -0700

    Letters now produce routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div>
+    <input type="text" class="ms-search" v-model="search" placeholder="Search for a meal">
+  </div>
+  <div class="ms-cards">
+    <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+      {{ letter }}
+    </router-link>
+  </div>
 </template>
 
 <script setup>
@@ -7,5 +14,6 @@ import { computed } from 'vue';
 import store from '../store';
 
 const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOP'.split('');
 
 </script>
