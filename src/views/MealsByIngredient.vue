b5d709a824dbe60d995680ceae09a9ac7b5b9843
Paul Miller <paulmiller@Pauls-MacBook-Pro.local>
2023-03-17 13:50:34 -0600

    .env file created

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -6,4 +6,16 @@
 
 <script setup>
 
+import { onMounted } from 'vue';
+import axiosClient from '@/axiosClient';
+
+onMounted(() => {
+  axiosClient.get('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 </script>
